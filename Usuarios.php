ef47ba87f94dde490b7d3cc9696da1d56d04b6e8
ruben <ruben7461@gmail.com>
2018-05-17 15:51:17 +0200

    modificacion consulta usuarios y creacion de clase Usuarios

--- Usuarios.php
+++ Usuarios.php
@@ -21,7 +21,7 @@ class Usuarios {
     function __construct($nombreUsuario,$apellidoUsuario,$fotoUsuario) {
         $this->nombreUsuario = $nombreUsuario;
         $this->apellidoUsuario = $apellidoUsuario;
-       // $this->nacionalidad = $nacionalidad;
+       //$this->nacionalidad = $nacionalidad;
         $this->fotoUsuario = $fotoUsuario;
        
     }
